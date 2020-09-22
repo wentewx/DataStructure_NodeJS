e7014fd96436417a51f147dd0122c7557046b06d
kfhwang <kfhwang@gm.nutc.edu.tw>
2020-09-22 15:08:08 +0800

    format

--- Elevator_V2.js
+++ Elevator_V2.js
@@ -1,5 +1,5 @@
 // JS 程式模擬電梯運作 Version 2: 
-// 練習while 迴圈、if、try...execpt語法
+// 練習while 迴圈、if、for
 // 使用 setTimeout，readline 套件 
 
 const readline = require('readline-sync');
@@ -11,45 +11,40 @@ function sleep(n) {//sleep n miliseconds
 
 var current_floor = 5; //電梯目前位於5樓
 var target_floor; //欲到達樓層 
- 
-do{ //無窮迴圈，迴圈內程式(6~37行)有break指令時可終止迴圈
+
+do { //無窮迴圈，迴圈內程式(6~37行)有break指令時可終止迴圈
     //使用者輸入欲到達樓層 
-    target_floor = readline.question('您現在在 ' + current_floor + ' 樓。請問要去哪一樓？' );
-     
-    try{ //try 搭配 catch 檢測之間的程式碼在執行時發生錯誤時由 catch 內的程式碼接手處理
-        target_floor = parseInt(target_floor) //若target_floor無法轉換成整數將產生ValueError錯誤
-    
-        if (isNaN(target_floor)|| target_floor < 1 || target_floor > 100){
-            console.log('請輸入介於 1~100的整數\n');
-            // console.log('請輸入介於 1~7 的整數');
-            continue;//繼續至第5行迴圈
+    target_floor = readline.question('您現在在 ' + current_floor + ' 樓。請問要去哪一樓？');
+
+    target_floor = parseInt(target_floor) //若target_floor無法轉換成整數將產生ValueError錯誤
+
+    if (isNaN(target_floor) || target_floor < 1 || target_floor > 100) {
+        console.log('請輸入介於 1~100的整數\n');
+        // console.log('請輸入介於 1~7 的整數');
+        continue;//繼續至第5行迴圈
+    }
+    if (target_floor == current_floor) {
+        console.log("離開電梯\n");
+        break; //終止 while (true) 迴圈
+    } else if (target_floor < current_floor) {
+        console.log('電梯下樓');
+        // while (target_floor < current_floor){ //迴圈當target_floor 大於或等於 current_floor時終止迴圈
+        //     console.log(current_floor); 
+        //     sleep(1000) 
+        //     current_floor -= 1; 
+        // }
+        //將上列while指令改用for loop，變數i從current_floor逐次減1至target_floor-1(即不含target_floor)
+        for (let i = current_floor; i > target_floor; i--) {
+            console.log(current_floor--);
+            sleep(100)//暫停0.1秒
         }
-        if (target_floor == current_floor){
-            console.log("離開電梯\n");
-            break; //終止 while (true) 迴圈
-        }else if (target_floor < current_floor){
-            console.log('電梯下樓');
-            // while (target_floor < current_floor){ //迴圈當target_floor 大於或等於 current_floor時終止迴圈
-            //     console.log(current_floor); 
-            //     sleep(1000) 
-            //     current_floor -= 1; 
-            // }
-            //將上列while指令改用for loop，變數i從current_floor逐次減1至target_floor-1(即不含target_floor)
-            for (let i = current_floor; i > target_floor; i--) {
-                console.log(current_floor--); 
-                sleep(100)//暫停0.1秒
-            }
-            console.log(current_floor);
-        }else{
-            console.log('電梯上樓');
-            for (let i = current_floor; i < target_floor; i++) {
-                console.log(current_floor++); 
-                sleep(100)//暫停0.1秒
-            }
-            console.log(current_floor);
+        console.log(current_floor);
+    } else {
+        console.log('電梯上樓');
+        for (let i = current_floor; i < target_floor; i++) {
+            console.log(current_floor++);
+            sleep(100)//暫停0.1秒
         }
-    }catch(ValueError){
-        console.log('格式錯誤，請輸入數字\n');
-    }  
-    
-}while(true);
+        console.log(current_floor);
+    }
+} while (true);
--- Hello.js
+++ Hello.js
@@ -6,7 +6,7 @@ var bmi = weight/((height/100)**2);
 
 console.log("Hello! Your BMI value is "+ bmi);
 
-// 使用第三方套件 readline-sync (待使用者輸入資料後程式再繼續執行) 
+//使用第三方套件 readline-sync (待使用者輸入資料後程式再繼續執行) 
 //NodeJS 套件管理程式為 npm，需打開終端機輸入指令
 //安裝 readline-sync 套件指令: npm install readline-sync
 //另可透過package.json指定所有相關套件後 npm install 即可安裝所有套件
@@ -19,10 +19,13 @@ height = Number(height);
 
 if(isNaN(weight) || isNaN(height))
     console.log("輸入資料有誤!");
+else if(weight<0 || weight>200 || height<0 || height>250)
+    console.log("輸入資料有誤!");
 else
     console.log("Your BMI value is "+ BMI(weight, height));
-
 
 function BMI(w, h){
     return Math.round(weight/((height/100)**2)*100)/100;
 }
+
+//not only bmi value, but also judgement
